b9b992f9956208defa0f43ca6344886a868548c4
Vynasso <vynasso.d@gmail.com>
2021-10-29 12:41:28 +0200

    dashboard in progress

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ var logger = require("morgan");
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 var inboxesRouter = require("./routes/inbox");
+var homeRouter = require("./routes/home");
+
 require("./models/connection");
 
 var app = express();
@@ -24,6 +26,8 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.use("/inbox", inboxesRouter);
+app.use("/home", homeRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- routes/home.js
+++ routes/home.js
@@ -4,15 +4,16 @@ var router = express.Router()
 var UserModel = require("../models/users")
 
 router.get("/userDetail", async (req, res) => {
-    try {
-        var user = await UserModel.findOne({
-            token: req.query.token,
-        });
-        res.send({ result: user, success: true });
-    } catch (error) {
-        res.status(404).json({ err: error, success: false });
+    var result = false;
+    var user = await UserModel.findOne({
+        token: req.query.token
+    })
+    if (user != null) {
+        result = true
     }
-    res.json({ result })
-});
+    console.log(result)
+    console.log(user)
+    res.json({ result, user })
+})
 
 module.exports = router
