cadcf643289d52c34725d5a46d53111a47c97cf0
Matthieu Petruzzi <matthieu.petruzzi@gmail.com>
2021-10-29 16:14:14 +0200

    avant push

--- routes/index.js
+++ routes/index.js
@@ -1,36 +1,39 @@
-var mongoose = require("mongoose");
+var mongoose = require("mongoose")
 
-var express = require("express");
-var router = express.Router();
-var UserModel = require("../models/users");
-const { ObjectId } = require("mongodb");
+var express = require("express")
+var router = express.Router()
+var UserModel = require("../models/users")
+const { ObjectId } = require("mongodb")
 
 router.post("/addquest", async function (req, res, next) {
-  console.log(req.body.quest);
+  console.log(req.body.quest)
   var user = await UserModel.findOne({
     token: req.body.token,
-  });
+  })
 
-  user.quests.push(req.body.quest);
+  user.quests.push(req.body.quest)
 
-  var userSaved = await user.save();
+  var userSaved = await user.save()
 
   if (userSaved) {
-    result = true;
+    result = true
   } else {
-    result = false;
+    result = false
   }
 
-  res.json({ result });
-});
+  res.json({ result })
+})
 
 router.get("/results", async function (req, res, next) {
-  var token = req.query.token;
-  var quest_id = req.query.quest_id;
+  var token = req.query.token
+  var quest_id = req.query.quest_id
 
-  var quest = await UserModel.findOne({ token: token }, { quests: { $elemMatch: { _id: quest_id } } });
-  quest = quest.quests[0];
-  console.log(quest);
+  var quest = await UserModel.findOne(
+    { token: token },
+    { quests: { $elemMatch: { _id: quest_id } } }
+  )
+  quest = quest.quests[0]
+  console.log(quest)
 
   //On créé le tableau de condition avant de lancer la requête car on souhaite le modifier dynamiquement pour les checkbox is_new, is_old et si on a une market_date
   var options = {
@@ -56,21 +59,21 @@ router.get("/results", async function (req, res, next) {
     "offers.terrace": quest.terrace,
     "offers.is_online": true,
     "offers.is_sold": false,
-  };
+  }
 
   //Si on cherche du neuf et du vieux on ajoute une condition "or"
   if ((quest.is_new && quest.is_old) || (!quest.is_new && !quest.is_old)) {
-    options["$or"] = [{ "offers.is_new": true }, { "offers.is_old": true }];
+    options["$or"] = [{ "offers.is_new": true }, { "offers.is_old": true }]
   } else {
-    options["offers.is_new"] = quest.is_new;
-    options["offers.is_old"] = quest.is_old;
+    options["offers.is_new"] = quest.is_new
+    options["offers.is_old"] = quest.is_old
   }
   //Si on a une date market_date, on souhaite seulement les offres plus récente ou = à cette date
   if (quest.market_date) {
-    options["offers.created"] = { $gte: new Date(quest.market_date) };
+    options["offers.created"] = { $gte: new Date(quest.market_date) }
   }
 
-  console.log(options);
+  console.log(options)
 
   var listOffers = await UserModel.aggregate([
     {
@@ -99,51 +102,56 @@ router.get("/results", async function (req, res, next) {
         "offers.created": 1,
       },
     },
-  ]);
+  ])
 
-  res.json({ listOffers, quest });
-});
+  res.json({ listOffers, quest })
+})
 
 router.get("/display-offer", async function (req, res, next) {
-  var id = req.query.offerId;
+  var id = req.query.offerId
+  console.log("offerId", id)
 
-  var offer = await UserModel.findOne({ "offers._id": id }, { offers: { $elemMatch: { _id: ObjectId(id) } } });
+  var offer = await UserModel.findOne(
+    { "offers._id": id },
+    { offers: { $elemMatch: { _id: ObjectId(id) } } }
+  )
+  console.log(offer)
   // on sélectionne uniquement les données de l'annonce
-  let offerData = offer.offers[0];
+  let offerData = offer.offers[0]
 
   // je vais récupérer le firstname, le is_pro et l'avatar du user qui a publié l'offre
-  var sellerId = offer._id;
-  var seller = await UserModel.findOne({ _id: sellerId });
-  console.log(seller.is_pro);
+  var sellerId = offer._id
+  var seller = await UserModel.findOne({ _id: sellerId })
+  console.log(seller.is_pro)
   let sellerData = {
     sellerToken: seller.token,
     sellerId: seller._id,
     firstName: seller.firstName,
     avatar: seller.avatar,
     is_pro: seller.is_pro,
-  };
-
-  // on renvoie au front les infos de l'annonce, et les infos du vendeur
-  res.json({ offerData, sellerData });
-});
-
-router.post("/addoffer", async function (req, res, next) {
-  console.log(req.body.offer);
-  var user = await UserModel.findOne({
-    token: req.body.token,
-  });
-
-  user.offers.push(req.body.offer);
-
-  var userSaved = await user.save();
-
-  if (userSaved) {
-    result = true;
-  } else {
-    result = false;
   }
 
-  res.json({ result });
-});
+  // on renvoie au front les infos de l'annonce, et les infos du vendeur
+  res.json({ offerData, sellerData })
+})
 
-module.exports = router;
+router.post("/addoffer", async function (req, res, next) {
+  console.log(req.body.offer)
+  var user = await UserModel.findOne({
+    token: req.body.token,
+  })
+
+  user.offers.push(req.body.offer)
+
+  var userSaved = await user.save()
+
+  if (userSaved) {
+    result = true
+  } else {
+    result = false
+  }
+
+  res.json({ result })
+})
+
+module.exports = router
