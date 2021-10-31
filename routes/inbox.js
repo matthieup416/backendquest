610fadb2456c92fc563088ce36e36c964d46c338
Matthieu Petruzzi <matthieu.petruzzi@gmail.com>
2021-10-31 23:49:09 +0100

    m

--- routes/inbox.js
+++ routes/inbox.js
@@ -1,20 +1,22 @@
-var express = require("express");
-var router = express.Router();
-var UserModel = require("../models/users");
-var ConversationModel = require("../models/conversations");
-const { ObjectId } = require("mongodb");
+var express = require("express")
+var router = express.Router()
+var UserModel = require("../models/users")
+var ConversationModel = require("../models/conversations")
+const { ObjectId } = require("mongodb")
 
 //Route qui récupère toutes les quêtes de l'utilisateur pour créer le picker avec la liste des quêtes
 router.get("/", async function (req, res, next) {
-  var token = req.query.token;
+  var token = req.query.token
   // chercher toutes les quetes de l'uilisateur et on ne selectionne que les champs dont on a besoin
-  var listQuest = await UserModel.find({ token: token }).select("quests._id quests.city quests.min_price quests.max_price");
-  res.json({ listQuest: listQuest[0].quests });
-});
+  var listQuest = await UserModel.find({ token: token }).select(
+    "quests._id quests.city quests.min_price quests.max_price"
+  )
+  res.json({ listQuest: listQuest[0].quests })
+})
 
 router.get("/selectedQuest", async function (req, res, next) {
-  var id = req.query.id; //Quest_id
-  var token = req.query.token;
+  var id = req.query.id //Quest_id
+  var token = req.query.token
   //On récupére toutes les conversations, avec le dernier messages, le nom de l'utilisateur du dernier message.
 
   var conversations = await ConversationModel.aggregate([
@@ -56,16 +58,16 @@ router.get("/selectedQuest", async function (req, res, next) {
         },
       },
     },
-  ]);
+  ])
 
-  console.log("conversations", conversations);
+  console.log("conversations", conversations)
 
-  res.json({ conversations });
-});
+  res.json({ conversations })
+})
 
 router.get("/conversation", async function (req, res, next) {
-  var id = req.query.id;
-  var token = req.query.token;
+  var id = req.query.id
+  var token = req.query.token
   //On récupère tous les messages de la conversation selectionnée
   var messages = await ConversationModel.aggregate([
     // On récupère le document ayant l'id du Get
@@ -92,50 +94,53 @@ router.get("/conversation", async function (req, res, next) {
         "users.avatar": 1,
       },
     },
-  ]);
+  ])
 
-  var offer = await UserModel.findOne({ token: messages[0].seller_token }, { offers: { $elemMatch: { _id: messages[0].offer_id } } });
+  var offer = await UserModel.findOne(
+    { token: messages[0].seller_token },
+    { offers: { $elemMatch: { _id: messages[0].offer_id } } }
+  )
 
   //On met tout en forme dans un objet à envoyer au front
   var messages = {
     listMessages: messages,
     offer: offer.offers,
-  };
-  res.json({ messages });
-});
+  }
+  res.json({ messages })
+})
 
 router.post("/addMessage", async function (req, res, next) {
   //user (sender), user (receiver), message, conversation id
-  console.log(req.body);
-  var newMessage = null;
+  console.log(req.body)
+  var newMessage = null
   if (req.body.id) {
-    var newMessage = await ConversationModel.findOne({ _id: req.body.id });
+    var newMessage = await ConversationModel.findOne({ _id: req.body.id })
   }
-  console.log("newMessage", newMessage);
+  console.log("newMessage", newMessage)
   if (newMessage === null) {
     var newMessage = new ConversationModel({
       accepted: false,
-      buyer_token: req.body.buyer_token,
-      seller_token: req.body.seller_token,
+      buyer_token: req.body.sender_token,
+      seller_token: req.body.receiver_token,
       quest_id: req.body.quest_id,
       offer_id: req.body.offer_id,
-    });
-    console.log("newMessage2", newMessage);
+    })
+    console.log("newMessage2", newMessage)
   }
   newMessage.messages.push({
     sender_token: req.body.sender_token,
     text: req.body.message,
-  });
+  })
 
-  var messageSaved = await newMessage.save();
+  var messageSaved = await newMessage.save()
 
-  console.log("messageSaved", messageSaved);
-  var result = false;
+  console.log("messageSaved", messageSaved)
+  var result = false
   if (messageSaved) {
-    result = true;
+    result = true
   }
 
-  res.json({ result, messageSaved });
-});
+  res.json({ result, messageSaved })
+})
 
-module.exports = router;
+module.exports = router
--- routes/index.js
+++ routes/index.js
@@ -1,36 +1,41 @@
-var mongoose = require("mongoose");
+var mongoose = require("mongoose")
+var express = require("express")
+const axios = require("axios")
 
-var express = require("express");
-var router = express.Router();
-var UserModel = require("../models/users");
-const { ObjectId } = require("mongodb");
+var router = express.Router()
+var UserModel = require("../models/users")
+const { ObjectId } = require("mongodb")
+const { response } = require("express")
 
 router.post("/addquest", async function (req, res, next) {
-  console.log(req.body.quest);
+  console.log(req.body.quest)
   var user = await UserModel.findOne({
     token: req.body.token,
-  });
+  })
 
-  user.quests.push(req.body.quest);
+  user.quests.push(req.body.quest)
 
-  var userSaved = await user.save();
+  var userSaved = await user.save()
 
   if (userSaved) {
-    result = true;
+    result = true
   } else {
-    result = false;
+    result = false
   }
 
-  res.json({ result });
-});
+  res.json({ result })
+})
 
 router.get("/results", async function (req, res, next) {
-  var token = req.query.token;
-  var quest_id = req.query.quest_id;
+  var token = req.query.token
+  var quest_id = req.query.quest_id
 
-  var quest = await UserModel.findOne({ token: token }, { quests: { $elemMatch: { _id: quest_id } } });
-  quest = quest.quests[0];
-  console.log("quest", quest);
+  var quest = await UserModel.findOne(
+    { token: token },
+    { quests: { $elemMatch: { _id: quest_id } } }
+  )
+  quest = quest.quests[0]
+  console.log("quest", quest)
 
   //On créé le tableau de condition avant de lancer la requête car on souhaite le modifier dynamiquement pour les checkbox is_new, is_old et si on a une market_date
   var options = {
@@ -56,21 +61,21 @@ router.get("/results", async function (req, res, next) {
     "offers.terrace": quest.terrace,
     "offers.is_online": true,
     "offers.is_sold": false,
-  };
+  }
 
   //Si on cherche du neuf et du vieux on ajoute une condition "or"
   if ((quest.is_new && quest.is_old) || (!quest.is_new && !quest.is_old)) {
-    options["$or"] = [{ "offers.is_new": true }, { "offers.is_old": true }];
+    options["$or"] = [{ "offers.is_new": true }, { "offers.is_old": true }]
   } else {
-    options["offers.is_new"] = quest.is_new;
-    options["offers.is_old"] = quest.is_old;
+    options["offers.is_new"] = quest.is_new
+    options["offers.is_old"] = quest.is_old
   }
   //Si on a une date market_date, on souhaite seulement les offres plus récente ou = à cette date
   if (quest.market_date) {
-    options["offers.created"] = { $gte: new Date(quest.market_date) };
+    options["offers.created"] = { $gte: new Date(quest.market_date) }
   }
 
-  console.log(options);
+  console.log(options)
 
   var listOffers = await UserModel.aggregate([
     {
@@ -101,53 +106,183 @@ router.get("/results", async function (req, res, next) {
         "offers._id": 1,
       },
     },
-  ]);
+  ])
 
-  res.json({ listOffers, quest });
-});
+  res.json({ listOffers, quest })
+})
 
 router.get("/display-offer", async function (req, res, next) {
-  var id = req.query.offerId;
-  console.log("offerId", id);
+  var id = req.query.offerId
+  console.log("offerId", id)
 
-  var offer = await UserModel.findOne({ "offers._id": id }, { offers: { $elemMatch: { _id: ObjectId(id) } } });
-  console.log(offer);
+  var offer = await UserModel.findOne(
+    { "offers._id": id },
+    { offers: { $elemMatch: { _id: ObjectId(id) } } }
+  )
+  console.log(offer)
   // on sélectionne uniquement les données de l'annonce
-  let offerData = offer.offers[0];
+  let offerData = offer.offers[0]
 
   // je vais récupérer le firstname, le is_pro et l'avatar du user qui a publié l'offre
-  var sellerId = offer._id;
-  var seller = await UserModel.findOne({ _id: sellerId });
-  console.log(seller.is_pro);
+  var sellerId = offer._id
+  var seller = await UserModel.findOne({ _id: sellerId })
+  console.log(seller.is_pro)
   let sellerData = {
     sellerToken: seller.token,
     sellerId: seller._id,
     firstName: seller.firstName,
     avatar: seller.avatar,
     is_pro: seller.is_pro,
-  };
-
-  // on renvoie au front les infos de l'annonce, et les infos du vendeur
-  res.json({ offerData, sellerData });
-});
-
-router.post("/addoffer", async function (req, res, next) {
-  console.log(req.body.offer);
-  var user = await UserModel.findOne({
-    token: req.body.token,
-  });
-
-  user.offers.push(req.body.offer);
-
-  var userSaved = await user.save();
-
-  if (userSaved) {
-    result = true;
-  } else {
-    result = false;
   }
 
-  res.json({ result });
-});
+  // on renvoie au front les infos de l'annonce, et les infos du vendeur
+  res.json({ offerData, sellerData })
+})
 
-module.exports = router;
+router.post("/addoffer", async function (req, res, next) {
+  var user = await UserModel.findOne({
+    token: req.body.token,
+  })
+
+  /// On envoie le nom de la ville à l'API PositionStack pour récupérer les coordonnées GPS de la ville
+  // On utilise la librairie AXIOS pour faire des calls API depuis notre backend
+  const apiURL = `http://api.positionstack.com/v1/forward?access_key=2373330d53389309f778b537f08b4603&query=${req.body.offer.city}`
+  const apiResponse = await axios.get(apiURL)
+  var newOffer = req.body.offer
+  // Une fois les coordonnées de la ville récupérées, on va modifier de facon aléatoire ces coordonnées afin d'obtenir des points légèrement différents sur la carte
+  // création d'un entier aléatoire entre -15 et 15 qu'on utilisera ensuite pour ajouter ou retirer 1.5km à la latitude et à la longitude pour que les offres sur une même ville soient légérement distants sur une carte
+  function entierAleatoire(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min
+  }
+  var entier = entierAleatoire(-15, 15)
+  console.log(entier)
+  /// on sait que 0.001° = 111 m
+  var hundredMetersMultiplier = 0.001
+  var finalLatitude =
+    apiResponse.data.data[0].latitude + hundredMetersMultiplier * entier
+  var finalLongitude =
+    apiResponse.data.data[0].longitude + hundredMetersMultiplier * entier
+  console.log(finalLatitude)
+  console.log(finalLongitude)
+
+  newOffer.latitude = finalLatitude
+  newOffer.longitude = finalLongitude
+  /// Une fois toutes les infos rassemblées on push l'offre
+  user.offers.push(newOffer)
+
+  var userSaved = await user.save()
+
+  if (userSaved) {
+    result = true
+  } else {
+    result = false
+  }
+
+  res.json({ result })
+})
+
+router.get("/resultsmap", async function (req, res, next) {
+  var token = req.query.token
+  var quest_id = req.query.quest_id
+
+  var quest = await UserModel.findOne(
+    { token: token },
+    { quests: { $elemMatch: { _id: quest_id } } }
+  )
+  quest = quest.quests[0]
+  console.log("quest", quest)
+
+  /// On envoie le nom de la ville à l'API PositionStack pour récupérer les coordonnées GPS de la ville
+  // On utilise la librairie AXIOS pour faire des calls API depuis notre backend
+  const apiURL = `http://api.positionstack.com/v1/forward?access_key=2373330d53389309f778b537f08b4603&query=${quest.city}`
+  const apiResponse = await axios.get(apiURL)
+  console.log("apiResponse", apiResponse.data.data)
+  const cityCoord = apiResponse.data.data[0]
+  var latitudeMin = cityCoord.latitude - (quest.rayon * 0.01) / 1.11
+  var latitudeMax = cityCoord.latitude + (quest.rayon * 0.01) / 1.11
+  var longitudeMin = cityCoord.longitude - (quest.rayon * 0.01) / 1.11
+  var longitudeMax = cityCoord.longitude + (quest.rayon * 0.01) / 1.11
+  //On créé le tableau de condition avant de lancer la requête car on souhaite le modifier dynamiquement pour les checkbox is_new, is_old et si on a une market_date
+  var options = {
+    "offers.latitude": {
+      $gte: latitudeMin,
+      $lte: latitudeMax,
+    },
+    "offers.longitude": {
+      $gte: longitudeMin,
+      $lte: longitudeMax,
+    },
+    "offers.type": quest.type,
+    "offers.price": {
+      $gte: quest.min_price,
+      $lte: quest.max_price,
+    },
+    "offers.nb_pieces": {
+      $gte: quest.pieces_min,
+      $lte: quest.pieces_max,
+    },
+    "offers.surface": {
+      $gte: quest.min_surface,
+      $lte: quest.max_surface,
+    },
+    "offers.elevator": quest.elevator,
+    "offers.parking": quest.parking,
+    "offers.fiber_optics": quest.fiber_optics,
+    "offers.pool": quest.pool,
+    "offers.balcony": quest.balcony,
+    "offers.terrace": quest.terrace,
+    "offers.is_online": true,
+    "offers.is_sold": false,
+  }
+
+  //Si on cherche du neuf et du vieux on ajoute une condition "or"
+  if ((quest.is_new && quest.is_old) || (!quest.is_new && !quest.is_old)) {
+    options["$or"] = [{ "offers.is_new": true }, { "offers.is_old": true }]
+  } else {
+    options["offers.is_new"] = quest.is_new
+    options["offers.is_old"] = quest.is_old
+  }
+  //Si on a une date market_date, on souhaite seulement les offres plus récente ou = à cette date
+  if (quest.market_date) {
+    options["offers.created"] = { $gte: new Date(quest.market_date) }
+  }
+
+  console.log(options)
+
+  var listOffers = await UserModel.aggregate([
+    {
+      $project: {
+        firstName: 1,
+        is_pro: 1,
+        verified: 1,
+        offers: 1,
+      },
+    },
+    { $unwind: { path: "$offers" } },
+    {
+      $match: options,
+    },
+    { $sort: { "offers.created": -1 } },
+    {
+      $project: {
+        firstName: 1,
+        is_pro: 1,
+        verified: 1,
+        "offers.city": 1,
+        "offers.type": 1,
+        "offers.nb_pieces": 1,
+        "offers.price": 1,
+        "offers.surface": 1,
+        "offers.created": 1,
+        "offers.pictures": 1,
+        "offers._id": 1,
+        "offers.latitude": 1,
+        "offers.longitude": 1,
+      },
+    },
+  ])
+  console.log("cityCoord", cityCoord)
+  res.json({ listOffers, quest, cityCoord })
+})
+
+module.exports = router
